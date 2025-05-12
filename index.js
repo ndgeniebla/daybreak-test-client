c8bb7661386caf35d37f6f3d0d0f59fce018e116
Neil Geniebla <ndgeniebla2@gmail.com>
2025-05-12 14:59:07 -0700

    change fetch to session

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 //     withCredentials: true
 // });
 
+fetch("https://daybreak-server.onrender.com/session", { credentials: "include" });
+
+const socket = io("https://daybreak-server.onrender.com", {
+    withCredentials: true,
+    transports: ["polling"]
+});
 
 let playerId = "";
 let playerDetails;
@@ -16,56 +22,42 @@ let gameBoard;
 
 let joinedLobbyId = "";
 
+window.addEventListener("load", e => {
+    // Array.from(document.querySelectorAll("input")).forEach(input => input.value = "");
+    socket.emit("getLobbyList");
+    socket.emit("rejoinLobby");
+})
 
-async function main() {
-    await fetch("https://daybreak-server.onrender.com/session", {
-        credentials: "include"
-    });
 
-    const socket = io("https://daybreak-server.onrender.com", {
-        withCredentials: true,
-        transports: ["polling"]
-    });
+socket.on("getPlayerId", pid => {
+    playerId = pid;
+    document.querySelector(".playerId").textContent = playerId;
+}) 
 
-    window.addEventListener("load", e => {
-        // Array.from(document.querySelectorAll("input")).forEach(input => input.value = "");
-        socket.emit("getLobbyList");
-        socket.emit("rejoinLobby");
+const createLobbyBtn = document.querySelector(".createLobbyBtn");
+createLobbyBtn.addEventListener("click", e => {
+    console.log("create lobby btn clicked");
+    socket.emit("createLobby");
+    socket.emit("getLobbyList");
+})
+
+const joinLobbyForm = document.querySelector(".joinLobbyForm");
+const roomCodeInput = document.querySelector(".roomCodeInput");
+
+joinLobbyForm.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log(e.currentTarget.value);
+    socket.emit("joinLobby", roomCodeInput.value);
+    socket.emit("getLobbyList");
+});
+
+const lobbiesDiv = document.querySelector(".lobbies");
+socket.on("receiveLobbyList", (lobbyList) => {
+    console.log(lobbyList)
+    lobbiesDiv.innerHTML = "";
+    lobbyList.forEach(lobbyDetails => {
+        const p = document.createElement("p");
+        p.textContent = lobbyDetails;
+        lobbiesDiv.append(p);
     })
-
-
-    socket.on("getPlayerId", pid => {
-        playerId = pid;
-        document.querySelector(".playerId").textContent = playerId;
-    }) 
-
-    const createLobbyBtn = document.querySelector(".createLobbyBtn");
-    createLobbyBtn.addEventListener("click", e => {
-        console.log("create lobby btn clicked");
-        socket.emit("createLobby");
-        socket.emit("getLobbyList");
-    })
-
-    const joinLobbyForm = document.querySelector(".joinLobbyForm");
-    const roomCodeInput = document.querySelector(".roomCodeInput");
-
-    joinLobbyForm.addEventListener("submit", e => {
-        e.preventDefault();
-        console.log(e.currentTarget.value);
-        socket.emit("joinLobby", roomCodeInput.value);
-        socket.emit("getLobbyList");
-    });
-
-    const lobbiesDiv = document.querySelector(".lobbies");
-    socket.on("receiveLobbyList", (lobbyList) => {
-        console.log(lobbyList)
-        lobbiesDiv.innerHTML = "";
-        lobbyList.forEach(lobbyDetails => {
-            const p = document.createElement("p");
-            p.textContent = lobbyDetails;
-            lobbiesDiv.append(p);
-        })
-    })
-}
-
-main();
+})
